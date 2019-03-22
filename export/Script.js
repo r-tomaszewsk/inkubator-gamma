c749a3693db0515b49c656cbacac63927948acb9
SRumak <rumak.sebastian@gmail.com>
2019-03-22 14:02:18 +0100

    FIXES fixes fixes

--- export/Script.js
+++ export/Script.js
@@ -2,16 +2,9 @@ const model = document.getElementById("model");
 const back = document.getElementById("back");
 const body = document.querySelector("body")
 
-const modelCode = document.getElementById("input");
-
 model.addEventListener("click", function(){
     body.classList.remove("firstScreen");
-    body.classList.add("secondScreen")
-
-    
-
-
-
+    body.classList.add("secondScreen");
 });
 
 back.addEventListener("click", function() {
